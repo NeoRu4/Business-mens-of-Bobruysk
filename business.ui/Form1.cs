8561adf0c45562437c5da26fb193af22c590f0bd
neoru4 <ibiws0373@yandex.ru>
2018-04-01 11:06:04 +0500

    Добавьте файлы проекта.

---
+++ business.ui/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+
+namespace business.ui
+{
+	public partial class Form1 : Form
+	{
+		public string CurFileName = "base.bu";
+		public List<Subject> businesMens;
+		public int countElements { get; private set; }
+
+		private void addToViewList(Subject sbj)
+		{
+			businesMens.Add(sbj);
+			listView1.Items.Add(new ListViewItem(new string[] { sbj.ITaxNum.ToString(), sbj.Name, sbj.StartDate.ToString("dd.MM.yyyy") }));
+			countElements++;
+		}
+
+		public Form1()
+		{
+			businesMens = new List<Subject>();
+			countElements = 0;
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			openFileDialog1.FileName = CurFileName;
+			openFileDialog1.Filter = "Файл базы (*.bu)|*.bu";
+			openFileDialog1.InitialDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+
+			listView1.Columns.Add("1", "ИНН", 100);
+			listView1.Columns.Add("2", "ФИО",250);
+			listView1.Columns.Add("3", "дата регестрации", 130);
+
+			button3.Enabled = false;
+
+		}
+
+		private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+		{
+			this.Activate();
+
+			string fileName = openFileDialog1.FileNames[0];
+			List<Subject> buffer = new List<Subject>();
+			Serializator.ReadFile(fileName, ref buffer);
+
+			foreach (var v in buffer)
+				addToViewList(v);
+
+
+			CurFileName = fileName;
+			openFileDialog1.FileName = CurFileName;
+			
+		}
+
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			openFileDialog1.ShowDialog();
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			var inforow = new addInfo();
+			var dialog = inforow.ShowDialog(this);
+			if (dialog == DialogResult.OK)
+			{
+				addToViewList(inforow.mainSubject);
+			}
+
+		}
+
+
+		private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			button3.Enabled = true;
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			var id = listView1.SelectedItems[0].Index;
+			var inforow = new addInfo(businesMens[id]);
+			var dialog = inforow.ShowDialog(this);
+			if (dialog == DialogResult.OK)
+			{
+				businesMens[id] = inforow.mainSubject;
+				listView1.Items[id] = new ListViewItem(new string[] { businesMens[id].ITaxNum.ToString(), businesMens[id].Name, businesMens[id].StartDate.ToString("dd.MM.yyyy") });
+			}
+
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			Serializator.WriteFile(CurFileName, businesMens);
+			MessageBox.Show(CurFileName+" сохранён", "Сохранение", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+		}
+
+
+	}
+}
---
+++ business.ui/addInfo.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace business.ui
+{
+	partial class addInfo
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(64, 280);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(443, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "продолжить";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// addInfo
+			// 
+			this.ClientSize = new System.Drawing.Size(582, 315);
+			this.Controls.Add(this.button1);
+			this.Font = new System.Drawing.Font("Arial Unicode MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Name = "addInfo";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Добавление/Изменение";
+			this.Load += new System.EventHandler(this.addInfo_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+	}
+}
